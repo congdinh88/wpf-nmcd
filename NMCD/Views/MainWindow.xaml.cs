0865b3839edc9d03c09deb4debd8f81dc78e1108
conding88 <congdinh.tk@gmail.com>
2024-05-22 22:48:10 +0700

    22/05/24-2nd

--- NMCD/Views/MainWindow.xaml.cs
+++ NMCD/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +17,39 @@ namespace NMCD
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<string> _items;
         public MainWindow()
         {
             InitializeComponent();
+            LoadComboBoxItems();
         }
+
+        private void LoadComboBoxItems()
+        {
+            _items = new ObservableCollection<string>
+            {
+                "Apple",
+                "Banana",
+                "Cherry",
+                "Date",
+                "Fig",
+                "Grape",
+                "Honeydew"
+            };
+
+            comboBox.ItemsSource = _items;
+        }
+
+        private void comboBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+            string typedText = comboBox.Text.ToString();
+
+                ObservableCollection<string> filteredItems = new ObservableCollection<string>(_items.Where(i=>i.ToLower().Contains(typedText.ToLower())));
+                comboBox.ItemsSource = filteredItems;
+                comboBox.IsDropDownOpen = true;
+
+        }
+       
     }
 }
